2b96aa68718d5c017e64d06656f470af89c142e3
Rui Lenovo <Rui Lenovo@RUILENOVO-PC>
2016-04-16 19:09:30 -0300

    Add project files.

---
+++ ContosoSite/Models/PartialClasses.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoSite.Models
+{
+    [MetadataType(typeof(StudentMetadata))]
+    public partial class Student
+    {
+    }
+
+    [MetadataType(typeof(EnrollmentMetadata))]
+    public partial class Enrollment
+    {
+    }
+
+}
